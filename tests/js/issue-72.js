bd8a4fe678513c994b39a97b99068c19139029f7
S'pht'Kr <sphtkr@icloud.com>
2017-01-09 06:44:08 +0100

    Fixed test for Issue #72

--- tests/js/issue-72.js
+++ tests/js/issue-72.js
@@ -1,6 +1,10 @@
 var homebridge = require("../../node_modules/homebridge/lib/api.js");
 
 test("Issue 72", function() {
+  var api = new homebridge.API();
+  _initializer(api);
+  var Service = api.hap.Service;
+  var Characteristic = api.hap.Characteristic;
   var devicesJson = {"data":{"structureChanged":true,"updateTime":1483686127,"devices":[
     {
       "creationTime":1481040700,
@@ -22,8 +26,6 @@ test("Issue 72", function() {
       "updateTime":1483686091
     }
   ]},"code":200,"message":"200 OK","error":null};
-  var api = new homebridge.API();
-  _initializer(api);
   var testPlatform = new platform({}, console.log);
   var foundAccessories = testPlatform.buildAccessoriesFromJson(devicesJson);
   assert.equal(foundAccessories.length, 1, "buildAccessoriesFromJson must find exactly one accessory.");
@@ -31,5 +33,9 @@ test("Issue 72", function() {
   var services = acc.getServices();
   assert.equal(services.length, 2, "getServices must return two services.");
   //console.log(JSON.stringify(services[1].characteristics, null, 4));
-
+  var wcServices = services.filter(function(service){ return service.UUID === Service.WindowCovering.UUID; });
+  assert.equal(wcServices.length, 1, "getServices must return exactly one WindowCovering service");
+  var tpCxs = wcServices[0].characteristics.filter(function(cx){ return cx.UUID === Characteristic.TargetPosition.UUID; });
+  assert.equal(tpCxs.length, 1, "The WindowCovering service must have exactly one TargetPosition Characteristic");
+  assert.strictEqual(tpCxs[0].props['maxValue'], 100, "The TargetPosition Characteristic must equal numeric 100");
 });
