5661f9e567b8c47d7c1c669cff2cbf9519d40c14
S'pht'Kr <sphtkr@icloud.com>
2017-01-10 07:36:38 +0100

    More test awesome(ish)ness

--- tests/js/issue-72.js
+++ tests/js/issue-72.js
@@ -1,10 +1,12 @@
 var homebridge = require("../../node_modules/homebridge/lib/api.js");
 
-test("Issue 72", function() {
-  var api = new homebridge.API();
-  _initializer(api);
-  var Service = api.hap.Service;
-  var Characteristic = api.hap.Characteristic;
+var api = new homebridge.API();
+_initializer(api);
+var testPlatform = new platform({}, console.log);
+var Service = api.hap.Service;
+var Characteristic = api.hap.Characteristic;
+
+test("Issue 72 maxValue", function() {
   var devicesJson = {"data":{"structureChanged":true,"updateTime":1483686127,"devices":[
     {
       "creationTime":1481040700,
@@ -26,7 +28,6 @@ test("Issue 72", function() {
       "updateTime":1483686091
     }
   ]},"code":200,"message":"200 OK","error":null};
-  var testPlatform = new platform({}, console.log);
   var foundAccessories = testPlatform.buildAccessoriesFromJson(devicesJson);
   assert.equal(foundAccessories.length, 1, "buildAccessoriesFromJson must find exactly one accessory.");
   var acc = new accessory(foundAccessories[0].name, foundAccessories[0].devDesc, testPlatform);
@@ -38,4 +39,34 @@ test("Issue 72", function() {
   var tpCxs = wcServices[0].characteristics.filter(function(cx){ return cx.UUID === Characteristic.TargetPosition.UUID; });
   assert.equal(tpCxs.length, 1, "The WindowCovering service must have exactly one TargetPosition Characteristic");
   assert.strictEqual(tpCxs[0].props['maxValue'], 100, "The TargetPosition Characteristic must equal numeric 100");
+});
+
+test("Issue 72 update", function() {
+  var devicesJson = {"data":{"structureChanged":true,"updateTime":1483686137,"devices":[
+    {
+      "creationTime":1481040700,
+      "creatorId":1,
+      "deviceType":"switchMultilevel",
+      "h":732948197,
+      "hasHistory":false,
+      "id":"ZWayVDev_zway_180-0-38",
+      "location":1,
+      "metrics":{
+        "icon":"blinds",
+        "title":"Blind (180.0)",
+        "level":100
+      },
+      "permanently_hidden":false,
+      "probeType":"motor",
+      "tags":[],
+      "visibility":true,
+      "updateTime":1483686136
+    }
+  ]},"code":200,"message":"200 OK","error":null};
+  testPlatform.processPollUpdate(devicesJson);
+  var cxs = testPlatform.cxVDevMap[devicesJson.data.devices[0].id];
+  var tpcx = cxs.filter(function(cx){ return cx.UUID === Characteristic.TargetPosition.UUID; })[0];
+  assert.equal(tpcx.value, 100, "Characteristic TargetPosition updates to value 100");
+  var cpcx = cxs.filter(function(cx){ return cx.UUID === Characteristic.CurrentPosition.UUID; })[0];
+  assert.equal(cpcx.value, 100, "Characteristic CurrentPosition updates to value 100");
 });
--- tests/js/switchBinary.js
+++ tests/js/switchBinary.js
@@ -1,5 +1,11 @@
 var homebridge = require("../../node_modules/homebridge/lib/api.js");
 
+var api = new homebridge.API();
+_initializer(api);
+var testPlatform = new platform({}, console.log);
+var Service = api.hap.Service;
+var Characteristic = api.hap.Characteristic;
+
 test("switchBinary", function() {
   var devicesJson = {"data":{"structureChanged":true,"updateTime":1483686127,"devices":[
     {
@@ -22,9 +28,6 @@ test("switchBinary", function() {
       "updateTime": 1483864350
     }
   ]},"code":200,"message":"200 OK","error":null};
-  var api = new homebridge.API();
-  _initializer(api);
-  var testPlatform = new platform({}, console.log);
   var foundAccessories = testPlatform.buildAccessoriesFromJson(devicesJson);
   assert.equal(foundAccessories.length, 1, "buildAccessoriesFromJson must find exactly one accessory.");
   var acc = new accessory(foundAccessories[0].name, foundAccessories[0].devDesc, testPlatform);
@@ -33,3 +36,31 @@ test("switchBinary", function() {
   //console.log(JSON.stringify(services[1].characteristics[1], null, 4));
   assert.equal(services[1].characteristics[1].UUID, api.hap.Characteristic.On.UUID, 'services[1].characteristics[1] should be an "On" characteristic');
 });
+
+test("switchBinary update", function(){
+  var devicesJson = {"data":{"structureChanged":true,"updateTime":1483686137,"devices":[
+    {
+      "creationTime": 1453783771,
+      "creatorId": 2,
+      "deviceType": "switchBinary",
+      "h": 1078449915,
+      "hasHistory": true,
+      "id": "ZWayVDev_zway_2-0-37",
+      "location": 3,
+      "metrics": {
+        "icon": "switch",
+        "title": "Test Lamps",
+        "level": "on"
+      },
+      "permanently_hidden": false,
+      "probeType": "",
+      "tags": [],
+      "visibility": true,
+      "updateTime": 1483686136
+    }
+  ]},"code":200,"message":"200 OK","error":null};
+  testPlatform.processPollUpdate(devicesJson);
+  var cxs = testPlatform.cxVDevMap[devicesJson.data.devices[0].id];
+  var oncx = cxs.filter(function(cx){ return cx.UUID === Characteristic.On.UUID; })[0];
+  assert.strictEqual(oncx.value, true, "Characteristic On updates to value boolean true");
+})
